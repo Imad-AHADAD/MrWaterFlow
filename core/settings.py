45d582ed12d592d6892b9c0f4c9643a636de40ae
Mehdi-Elargoubi <mehdi.elargoubi11@gmail.com>
2024-06-05 07:38:40 +0100

    updating the home page

--- core/settings.py
+++ core/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(CORE_DIR, 'apps/static'),
+    os.path.join(BASE_DIR, 'apps/static'),
 )
 
 
